264aba61e237b0e270291cc72997e7e6dc062667
NHatThanh06 <92626962+NHatThanh06@users.noreply.github.com>
2023-05-11 09:38:51 +0700

    Add files via upload

---
+++ bt/bt6_3/Movable.java
@@ -0,0 +1,8 @@
+package bt.day4.bt6_3;
+
+public abstract class Movable {
+    abstract void moveUp();
+    abstract void moveDown();
+    abstract void moveLeft();
+    abstract void moveRight();
+}
