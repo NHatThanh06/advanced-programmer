fd05af77533bbefce9a2a06aa7d5acaa02d081c5
NHatThanh06 <92626962+NHatThanh06@users.noreply.github.com>
2023-05-11 09:35:14 +0700

    Add files via upload

---
+++ bt/bt6_2/GeometricObject.java
@@ -0,0 +1,7 @@
+package bt.day4;
+
+public interface GeometricObject {
+    public double getArea();
+    public double getPerimeter();
+
+}
---
+++ bt/bt6_2/main.java
@@ -0,0 +1,18 @@
+package bt.day4;
+
+public class main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(2.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+
+        double circleArea = circle.getArea();
+        double circlePerimeter = circle.getPerimeter();
+        double rectangleArea = rectangle.getArea();
+        double rectanglePerimeter = rectangle.getPerimeter();
+
+        System.out.println("Circle Area: " + circleArea);
+        System.out.println("Circle Perimeter: " + circlePerimeter);
+        System.out.println("Rectangle Area: " + rectangleArea);
+        System.out.println("Rectangle Perimeter: " + rectanglePerimeter);
+    }
+}
